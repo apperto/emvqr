c8449e48bde61affde1a7ac08f4dc7ddeced11d7
Facundo Medica <facundolmedica@gmail.com>
2019-03-21 13:39:36 +0000

    count runes/characters, not bytes

--- emvqr.go
+++ emvqr.go
@@ -38,21 +38,21 @@ func (data QRData) GenerateString() (string, error) {
 		customData := ""
 		for tag, value := range idValue {
 			customData += fmt.Sprintf("%02d", tag)
-			customData += fmt.Sprintf("%02d", len(value))
+			customData += fmt.Sprintf("%02d", len([]rune(value)))
 			customData += value
 		}
-		if len(customData) > 99 {
+		if len([]rune(customData)) > 99 {
 			return "", errors.New("custom identifier too long (max length: 99 characters)")
 		}
-		customData = fmt.Sprintf("%02d", identifier) + fmt.Sprintf("%02d", len(customData)) + customData
+		customData = fmt.Sprintf("%02d", identifier) + fmt.Sprintf("%02d", len([]rune(customData))) + customData
 		qr += customData
 	}
 
 	qr += "52" + fmt.Sprintf("%02d", len(data.MCC)) + data.MCC
 	qr += "5303" + data.Currency
 	qr += "5802" + data.Country
-	qr += "59" + fmt.Sprintf("%02d", len(data.MerchantName)) + data.MerchantName
-	qr += "60" + fmt.Sprintf("%02d", len(data.City)) + data.City
+	qr += "59" + fmt.Sprintf("%02d", len([]rune(data.MerchantName))) + data.MerchantName
+	qr += "60" + fmt.Sprintf("%02d", len([]rune(data.City))) + data.City
 	qr += "6304"
 	qr += crc16.Checksum([]byte(qr))
 
